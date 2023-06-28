12315ad9c7625683811d4902b6a23b7cb71c6719
dninosores <dninosores@gmail.com>
2023-06-27 20:19:34 -0400

    Moved menu item to be under "export" in the file menu

--- exportregion/exportregion.py
+++ exportregion/exportregion.py
@@ -2,6 +2,7 @@
 from krita import *
 from PyQt5.QtWidgets import QWidget, QAction
 from pprint import pprint
+from functools import partial
 
 
 class ExportRegionExtension(Extension):
@@ -41,6 +42,22 @@ class ExportRegionExtension(Extension):
             "dninosores_export_region", "Export Region...", "file"
         )
         export_region_action.triggered.connect(self.export_region)
+        QTimer.singleShot(
+            0, partial(self.moveAction, export_region_action, window.qwindow())
+        )
+
+    # Take the existing export_region action and move it to be after file_export in the file menu
+    def moveAction(self, action, qwindow):
+        menu_bar = qwindow.menuBar()
+        file_menu_action = next(
+            (a for a in menu_bar.actions() if a.objectName() == "file"), None
+        )
+        if file_menu_action:
+            file_menu = file_menu_action.menu()
+            for file_action in file_menu.actions():
+                if file_action.objectName() == "file_export_file":
+                    file_menu.removeAction(action)
+                    file_menu.insertAction(file_action, action)
 
 
 Krita.instance().addExtension(ExportRegionExtension(Krita.instance()))
