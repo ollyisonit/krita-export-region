57a11ccfe3f2ae02c57275e54bb215d4de1cc377
olly <>
2026-06-30 08:27:30 -0700

    Added deploy script

---
+++ deploy.bat
@@ -0,0 +1,16 @@
+set NAME=exportregion
+set ZIP="%PROGRAMFILES%\7-Zip\7z.exe"
+set KRITA="%PROGRAMFILES%\Krita (x64)\bin\krita.com"
+
+del /S /Q %appdata%\krita\pykrita\%NAME%
+del /Q %appdata%\krita\pykrita\%NAME%.desktop
+del /Q %appdata%\krita\actions\%NAME%.action
+del /Q .\%NAME%.zip
+
+robocopy .\%NAME% %appdata%\krita\pykrita\%NAME% /E
+robocopy . %appdata%\krita\pykrita %NAME%.desktop
+robocopy .\%NAME% %appdata%\krita\actions %NAME%.action
+
+%ZIP% a -tzip .\%NAME%.zip .\%NAME% .\%NAME%.desktop
+
+%KRITA%
